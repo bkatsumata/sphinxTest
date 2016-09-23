76047ee2badeb454f2e211b3c5ca6951d2f197d8
bkatsumata <bkatsumata@chromeriver.com>
2016-09-23 15:11:52 -0700

    commit 2

--- code/ModTest.py
+++ code/ModTest.py
@@ -1,14 +1,19 @@
 class Test1:
-	"""
-	This class does nothing
-	"""
-	def __init__(self):
-		pass
-	def function1(self,number):
-		"""
-		This functions adds 1
-		
-		:param number int: This is a number to add one
-		:rtype: bool
-		"""
-		return number+1
+    """
+    This class does nothing
+    """
+
+    def __init__(self):
+        pass
+
+    def function1(self, number):
+        """
+        This functions adds 1
+
+        :param number int: This is a number to add one
+        :rtype: bool
+        """
+        return number + 1
+
+    def addnew(self):
+        pass
